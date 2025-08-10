8bbe5d0c61a177d90b3d67d8b9a3da47e0b051e0
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-11 02:00:21 +0900

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,82 @@
+# name: discourse-advanced-raffle
+# about: A plugin to create advanced raffles/lotteries in Discourse topics.
+# version: 0.1
+# authors: Your Name / Company
+# url: https://github.com/your-repo/discourse-advanced-raffle
+
+enabled_site_setting :raffle_enabled
+
+# === 注册所有前端资源 ===
+register_asset "javascripts/discourse/initializers/add-raffle-composer-button.js.es6"
+register_asset "javascripts/discourse/components/raffle-editor.js"
+register_asset "javascripts/discourse/components/raffle-info-card.js"
+register_asset "stylesheets/common/raffle.scss"
+
+after_initialize do
+  # 依赖 Discourse 的 autoloading 机制加载 app 和 lib 目录下的文件
+
+  Topic.class_eval do
+    has_one :lottery_activity, class_name: "LotteryActivity", dependent: :destroy
+  end
+
+  module ::DiscourseAdvancedRaffle
+    class RafflesController < ::ApplicationController
+      requires_plugin 'discourse-advanced-raffle'
+      before_action :ensure_logged_in
+
+      def update
+        topic = Topic.find_by(id: params[:topic_id])
+        return render_json_error(I18n.t('topic.not_found'), status: 404) unless topic
+        guardian.ensure_can_edit!(topic)
+
+        activity = LotteryActivity.find_or_initialize_by(topic_id: topic.id)
+        activity.user_id = current_user.id
+
+        activity_params = params.require(:activity).permit(
+          :status,
+          :draw_type,
+          :start_time,
+          :end_time,
+          draw_condition: {},
+          participation_rules: {}
+        )
+        
+        prizes_params = params.require(:activity).permit(prizes: [:id, :name, :description, :quantity, :image_url])[:prizes] || []
+
+        ActiveRecord::Base.transaction do
+          activity.assign_attributes(activity_params)
+          activity.save!
+
+          activity.prizes.destroy_all
+          activity.prizes.create!(prizes_params) if prizes_params.present?
+        end
+
+        render json: LotteryActivitySerializer.new(activity, root: false).as_json
+      rescue ActiveRecord::RecordInvalid => e
+        render_json_error(e.message, status: 422)
+      end
+    end
+
+    class Engine < ::Rails::Engine
+      engine_name 'discourse_advanced_raffle'
+      isolate_namespace DiscourseAdvancedRaffle
+    end
+
+    Engine.routes.draw do
+      put '/:topic_id' => 'raffles#update'
+    end
+  end
+
+  Discourse::Application.routes.append do
+    mount DiscourseAdvancedRaffle::Engine, at: '/raffles'
+  end
+
+  require_dependency "topic_view_serializer"
+  add_to_serializer(:topic_view, :lottery_activity, false) do
+    LotteryActivitySerializer.new(object.topic.lottery_activity, root: false).as_json
+  end
+
+  add_to_serializer(:topic_view, :include_lottery_activity?) do
+    object.topic.lottery_activity.present?
+  end
+end
